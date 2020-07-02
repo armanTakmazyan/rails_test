2c742edc15994ec30c33d0b48eec433b1bda3747
Arman Takmazyan <armanworkmail01@gmail.com>
2020-07-02 11:42:23 +0400

    Added admin field to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_073456) do
+ActiveRecord::Schema.define(version: 2020_07_02_074027) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
 end
